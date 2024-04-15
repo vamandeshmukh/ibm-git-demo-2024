d1325b870b93fca84642d014ed53615baa858961
DiyaTakkar22 <166809636+DiyaTakkar22@users.noreply.github.com>
2024-04-15 10:29:38 +0530

    Update Hello.java
    changes

--- Hello.java
+++ Hello.java
@@ -3,7 +3,8 @@ public class Hello {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
-		System.out.println("Hello world! 2");
+		public String sayHello(){
+			System.out.println("hi! i am pleased to meet you ");
 	}
 }
 
@@ -11,22 +12,12 @@ public class Hello {
 
 /*
 
-For newly created repository - 
-------------------------------
 git init
 git add .
 git commit -m "first commit"
 git branch -M main
 git remote add origin https://github.com/vamandeshmukh/ibm-git-demo-2024.git
 git push -u origin main
-
-For subsequent changes in the code 
-----------------------------------
-
-git add . 
-git commit -m "some commit message"
-git push 
-
 
 */
 
